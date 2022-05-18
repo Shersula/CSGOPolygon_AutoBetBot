a57e4077e73c7cd50313f2f11573a9730713da2b
Shersula <samovarov2002@mail.ru>
2022-05-18 20:22:40 +0300

    Add select custom numbers

--- bot.py
+++ bot.py
@@ -21,6 +21,9 @@ class Window(QMainWindow):
 		self.InfoSet = False
 		self.Black = 0
 		self.Red = 0
+		self.Green = 0
+
+		self.checkbox_num = [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None]
 		super().__init__()
 		self.setObjectName("Window")
 		self.resize(699, 459)
@@ -60,6 +63,8 @@ class Window(QMainWindow):
 		self.comboBox.setObjectName("comboBox")
 		self.comboBox.addItem("")
 		self.comboBox.addItem("")
+		self.comboBox.addItem("")
+		self.comboBox.addItem("")
 		self.horizontalLayout_6.addWidget(self.comboBox)
 		spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
 		self.horizontalLayout_6.addItem(spacerItem1)
@@ -72,6 +77,75 @@ class Window(QMainWindow):
 		self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
 		self.pushButton_3.setObjectName("pushButton_3")
 		self.horizontalLayout_4.addWidget(self.pushButton_3)
+
+		self.horizontalLayout_11 = QtWidgets.QHBoxLayout()
+		self.horizontalLayout_11.setObjectName("horizontalLayout_11")
+		self.horizontalLayout_12 = QtWidgets.QHBoxLayout()
+		self.horizontalLayout_12.setObjectName("horizontalLayout_12")
+
+		self.checkbox_num[0] = QtWidgets.QCheckBox("0", self)
+		self.checkbox_num[0].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[1] = QtWidgets.QCheckBox("1", self)
+		self.checkbox_num[1].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[2] = QtWidgets.QCheckBox("2", self)
+		self.checkbox_num[2].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[3] = QtWidgets.QCheckBox("3", self)
+		self.checkbox_num[3].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[4] = QtWidgets.QCheckBox("4", self)
+		self.checkbox_num[4].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[5] = QtWidgets.QCheckBox("5", self)
+		self.checkbox_num[5].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[6] = QtWidgets.QCheckBox("6", self)
+		self.checkbox_num[6].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[7] = QtWidgets.QCheckBox("7", self)
+		self.checkbox_num[7].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[8] = QtWidgets.QCheckBox("8", self)
+		self.checkbox_num[8].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[9] = QtWidgets.QCheckBox("9", self)
+		self.checkbox_num[9].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[10] = QtWidgets.QCheckBox("10", self)
+		self.checkbox_num[10].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[11] = QtWidgets.QCheckBox("11", self)
+		self.checkbox_num[11].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[12] = QtWidgets.QCheckBox("12", self)
+		self.checkbox_num[12].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[13] = QtWidgets.QCheckBox("13", self)
+		self.checkbox_num[13].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.checkbox_num[14] = QtWidgets.QCheckBox("14", self)
+		self.checkbox_num[14].setToolTip('<b>Числа на которые должен ставить бот</b>')
+
+		self.horizontalLayout_11.addWidget(self.checkbox_num[0])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[1])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[2])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[3])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[4])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[5])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[6])
+		self.horizontalLayout_11.addWidget(self.checkbox_num[7])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[8])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[9])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[10])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[11])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[12])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[13])
+		self.horizontalLayout_12.addWidget(self.checkbox_num[14])
+
+		for i in range(len(self.checkbox_num)):
+			if self.checkbox_num[i] != None: self.checkbox_num[i].setVisible(False)
 
 		self.horizontalLayout_10 = QtWidgets.QHBoxLayout()
 		self.horizontalLayout_10.setObjectName("horizontalLayout_10")
@@ -97,6 +171,8 @@ class Window(QMainWindow):
 		spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
 		self.horizontalLayout_4.addItem(spacerItem2)
 		self.verticalLayout_2.addLayout(self.horizontalLayout_4)
+		self.verticalLayout_2.addLayout(self.horizontalLayout_11)
+		self.verticalLayout_2.addLayout(self.horizontalLayout_12)
 		self.verticalLayout_2.addLayout(self.horizontalLayout_10)
 		self.verticalLayout_2.addLayout(self.horizontalLayout_9)
 		self.verticalLayout_2.addLayout(self.horizontalLayout_8)
@@ -168,6 +244,8 @@ class Window(QMainWindow):
 		self.label.setText("Ставка")
 		self.comboBox.setItemText(0, "Черный")
 		self.comboBox.setItemText(1, "Красный")
+		self.comboBox.setItemText(2, "Зеленый")
+		self.comboBox.setItemText(3, "Конкретные числа")
 		self.pushButton_2.setText("Старт")
 		self.pushButton_3.setText("Стоп")
 		self.label_2.setText("Количество ставок")
@@ -183,6 +261,7 @@ class Window(QMainWindow):
 		self.pushButton_2.clicked.connect(self.StartBrowser)
 		self.pushButton_3.clicked.connect(self.StopBrowser)
 		self.pushButton_3.setEnabled(False)
+		self.comboBox.currentIndexChanged.connect(self.ShowCheckBoxNum)
 
 	def closeEvent(self, event):
 		if self.BetObj != None: self.StopBrowser()
@@ -192,6 +271,16 @@ class Window(QMainWindow):
 		self.ClearStatistic()
 		self.BetObj.StopBtn = True
 		self.label_3.setText(string + "\nКнопка старт станет доступна после закрытия окна браузера\nПеред тем как кнопка станет активна потребуется некоторое время\nчтобы бот завершил очистку всей системной информации")
+
+	def ShowCheckBoxNum(self, index):
+		if index == 3:
+			for i in range(len(self.checkbox_num)):
+				if self.checkbox_num[i] != None: self.checkbox_num[i].setVisible(True)
+		else:
+			for i in range(len(self.checkbox_num)):
+				if self.checkbox_num[i] != None:
+					self.checkbox_num[i].setCheckState(0)
+					self.checkbox_num[i].setVisible(False)
 
 	def ClearBet(self):
 		if self.BetObj.StopBtn == True:
@@ -245,6 +334,7 @@ class Window(QMainWindow):
 		self.label_5.setText("")
 		self.Black = 0
 		self.Red = 0
+		self.Green = 0
 
 	def UpdateInformation(self):
 		StrStartTime = self.StartTime.strftime('%d.%m.%Y %H:%M:%S')
@@ -254,7 +344,7 @@ class Window(QMainWindow):
 		self.label_4.setText(InfoString)
 
 	def UpdateStatistic(self):
-		StatString = "Число выпадений\nЧерное: " + str(self.Black) + " || Красное: " + str(self.Red)
+		StatString = "Число выпадений\nЧерное: " + str(self.Black) + " || Красное: " + str(self.Red) + " || Зеленое: " + str(self.Green)
 		self.label_5.setText(StatString)
 
 	def AutoSelectColor(self):
@@ -297,6 +387,11 @@ class Bet(QObject):
 		self.LooseCount = 0
 		self.WaitColor = False
 
+		self.NumberQueue = []
+		self.QueueBuild = False
+
+		self.WaitRolling = False
+
 	def StartBet(self):
 
 		if self.driver != None:
@@ -326,7 +421,7 @@ class Bet(QObject):
 		self.driver = webdriver.Ie(options=self.options)
 		"""
 
-		self.driver.get('https://csgopolygon.gg')
+		self.driver.get('https://plg.bet')
 		self.handle = self.driver.current_window_handle
 
 		if self.LoopTimer != None:
@@ -335,7 +430,7 @@ class Bet(QObject):
 		self.LoopTimer = QTimer()
 		self.LoopTimer.timeout.connect(self.Loop)
 
-		self.LoopTimer.start(500)
+		self.LoopTimer.start(1000)
 	
 	def Loop(self):
 		try:
@@ -358,7 +453,7 @@ class Bet(QObject):
 					del self.driver
 					self.driver = None
 				self.driver = webdriver.Chrome(service=self.path, options=self.options)
-				self.driver.get('https://csgopolygon.gg')
+				self.driver.get('https://plg.bet')
 				self.handle = self.driver.current_window_handle
 		if self.StopBtn != True:
 			try:
@@ -376,7 +471,8 @@ class Bet(QObject):
 						num = re.sub("[^0-9]", "", element.text)
 
 						if 7 >= int(num) >= 1: window.Red += 1
-						elif 14 >= int(num) >= 8: window.Black += 1 
+						elif 14 >= int(num) >= 8: window.Black += 1
+						elif int(num) == 0: window.Green += 1
 						self.WaitColor = False
 						window.UpdateStatistic()
 					except ValueError: pass
@@ -385,7 +481,7 @@ class Bet(QObject):
 				if window.comboBox.currentIndex() == 0: #black
 					if self.Bet == False:
 						element = self.driver.find_element(By.XPATH, "//*[@id='select_roulette']/div[1]/div[1]/span")
-						if element.text != "***ROLLING***":
+						if element.text != "***ROLLING***" and element.text != "***ВРАЩЕНИЕ***":
 							element = self.driver.find_element(By.XPATH, "//*[@id='black_bets_my']/span")
 							if(element.text == "0"):
 								element = self.driver.find_element(By.XPATH, "//input[@type='text'][@id='roulette_amount']")
@@ -416,14 +512,14 @@ class Bet(QObject):
 							self.WaitColor = True
 							if window.BigLoseStreak < self.LooseCount: window.BigLoseStreak = self.LooseCount
 						elif(element.text == "0"):
-							self.driver.get('https://csgopolygon.gg')
+							self.driver.get('https://plg.bet')
 							self.Bet = False
 							self.LastBetSet = False
 
 				elif window.comboBox.currentIndex() == 1: #red
 					if self.Bet == False:
 						element = self.driver.find_element(By.XPATH, "//*[@id='select_roulette']/div[1]/div[1]/span")
-						if element.text != "***ROLLING***":
+						if element.text != "***ROLLING***" and element.text != "***ВРАЩЕНИЕ***":
 							element = self.driver.find_element(By.XPATH, "//*[@id='red_bets_my']/span")
 							if(element.text == "0"):
 								element = self.driver.find_element(By.XPATH, "//input[@type='text'][@id='roulette_amount']")
@@ -454,12 +550,124 @@ class Bet(QObject):
 							self.WaitColor = True
 							if window.BigLoseStreak < self.LooseCount: window.BigLoseStreak = self.LooseCount
 						elif(element.text == "0"):
-							self.driver.get('https://csgopolygon.gg')
+							self.driver.get('https://plg.bet')
+							self.Bet = False
+							self.LastBetSet = False
+				elif window.comboBox.currentIndex() == 2: #green
+					if self.Bet == False:
+						element = self.driver.find_element(By.XPATH, "//*[@id='select_roulette']/div[1]/div[1]/span")
+						if element.text != "***ROLLING***" and element.text != "***ВРАЩЕНИЕ***":
+							element = self.driver.find_element(By.XPATH, "//*[@id='green_bets_my']/span")
+							if(element.text == "0"):
+								element = self.driver.find_element(By.XPATH, "//input[@type='text'][@id='roulette_amount']")
+								element.clear()
+								if self.LastBet == 0: element.send_keys(str(window.spinBox.value()))
+								else: element.send_keys(str(self.LastBet))
+
+								element = self.driver.find_element(By.XPATH, "//button[@type='button'][@name='button'][@class='green_button betButton']")
+								element.click()
+
+								element = self.driver.find_element(By.XPATH, "//*[@id='green_bets_my']/span")
+							else:
+								self.Bet = True
+								if self.LastBet == 0: self.LastBet = window.spinBox.value()
+					else:
+						element = self.driver.find_element(By.XPATH, "//*[@id='green_bets_my']/span")
+						if element.text.find("+") != -1 and self.LastBetSet == False:
+							self.LastBet = 0
+							window.Coins += window.spinBox.value()
+							self.LastBetSet = True
+							self.LooseCount = 0
+							self.WaitColor = True
+							if window.checkbox.isChecked(): window.AutoSelectColor()
+						elif element.text.find("-") != -1 and self.LastBetSet == False:
+							self.LastBet *= 2
+							self.LastBetSet = True
+							self.LooseCount += 1
+							self.WaitColor = True
+							if window.BigLoseStreak < self.LooseCount: window.BigLoseStreak = self.LooseCount
+						elif(element.text == "0"):
+							self.driver.get('https://plg.bet')
+							self.Bet = False
+							self.LastBetSet = False
+				elif window.comboBox.currentIndex() == 3: #numbers
+					if self.Bet == False:
+						element = self.driver.find_element(By.XPATH, "//*[@id='select_roulette']/div[1]/div[1]/span")
+						if (element.text.find("Вращение через") != -1 or element.text.find("Rolling in") != -1) and self.WaitRolling == True:
+							if len(self.NumberQueue) <= 0 and self.QueueBuild == False:
+								for i in range(len(window.checkbox_num)):
+									if window.checkbox_num[i].isChecked(): self.NumberQueue.append(i)
+								self.QueueBuild = True
+							elif len(self.NumberQueue) > 0:
+								if(self.NumberQueue[0] == 0):
+									element = self.driver.find_element(By.XPATH, "//input[@type='text'][@id='roulette_amount']")
+									element.clear()
+									if self.LastBet == 0: element.send_keys(str(window.spinBox.value()))
+									else: element.send_keys(str(self.LastBet))
+
+									element = self.driver.find_element(By.XPATH, "//button[@type='button'][@name='button'][@class='green_button betButton']")
+									element.click()
+
+									element = self.driver.find_element(By.XPATH, "//*[@id='green_bets_my']/span")
+									self.NumberQueue.remove(self.NumberQueue[0])
+								elif(1 <= self.NumberQueue[0] <= 7):
+									element = self.driver.find_element(By.XPATH, "//input[@type='text'][@id='roulette_amount']")
+									element.clear()
+									if self.LastBet == 0: element.send_keys(str(window.spinBox.value()))
+									else: element.send_keys(str(self.LastBet))
+
+									SubStr = "//span[@class='round_bets_item betButton'][@data-lower='"+ str(self.NumberQueue[0]) +"']"
+
+									element = self.driver.find_element(By.XPATH, SubStr)
+									element.click()
+
+									element = self.driver.find_element(By.XPATH, "//*[@id='red_bets_my']/span")
+									self.NumberQueue.remove(self.NumberQueue[0])
+								elif(8 <= self.NumberQueue[0] <= 14):
+									element = self.driver.find_element(By.XPATH, "//input[@type='text'][@id='roulette_amount']")
+									element.clear()
+									if self.LastBet == 0: element.send_keys(str(window.spinBox.value()))
+									else: element.send_keys(str(self.LastBet))
+
+									SubStr = "//span[@class='round_bets_item betButton'][@data-lower='"+ str(self.NumberQueue[0]) +"']"
+
+									element = self.driver.find_element(By.XPATH, SubStr)
+									element.click()
+
+									element = self.driver.find_element(By.XPATH, "//*[@id='black_bets_my']/span")
+									self.NumberQueue.remove(self.NumberQueue[0])
+							else:
+								self.NumberQueue = []
+								self.QueueBuild = False
+								self.Bet = True
+								if self.LastBet == 0: self.LastBet = window.spinBox.value()
+						elif (element.text == "***ROLLING***" or element.text == "***ВРАЩЕНИЕ***") and self.WaitRolling != True:
+							self.WaitRolling = True
+							self.driver.get('https://plg.bet')
+					else:
+						elementgreen = self.driver.find_element(By.XPATH, "//*[@id='green_bets_my']/span")
+						elementred = self.driver.find_element(By.XPATH, "//*[@id='red_bets_my']/span")
+						elementblack = self.driver.find_element(By.XPATH, "//*[@id='black_bets_my']/span")
+						if (elementgreen.text.find("+") != -1 or elementred.text.find("+") != -1 or elementblack.text.find("+") != -1) and self.LastBetSet == False:
+							self.LastBet = 0
+							window.Coins += window.spinBox.value()*14
+							self.LastBetSet = True
+							self.LooseCount = 0
+							self.WaitColor = True
+						elif (elementgreen.text.find("-") != -1 or elementred.text.find("-") != -1 or elementblack.text.find("-") != -1) and self.LastBetSet == False:
+							self.LastBet *= 2
+							self.LastBetSet = True
+							self.LooseCount += 1
+							self.WaitColor = True
+							if window.BigLoseStreak < self.LooseCount: window.BigLoseStreak = self.LooseCount
+						elif(elementgreen.text == "0" and elementred.text == "0" and elementblack.text == "0"):
+							self.driver.get('https://plg.bet')
 							self.Bet = False
 							self.LastBetSet = False
 
 			except exceptions.NoSuchElementException:
 				window.label_3.setText(string + "\nДля запуска бота вам нужно авторизироваться")
+			except exceptions.StaleElementReferenceException: pass
 
 def Application():
 	app = QApplication(sys.argv)
